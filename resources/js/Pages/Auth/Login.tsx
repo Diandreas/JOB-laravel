bdb57c6efe50dca65e35f667f9fef3a429ba2130
Diandreas <davidnjandjeu@gmail.com>
2024-05-03 01:48:02 +0100

    about us

--- resources/js/Pages/Auth/Login.tsx
+++ resources/js/Pages/Auth/Login.tsx
@@ -35,7 +35,7 @@ export default function Login({ status, canResetPassword }: { status?: string, c
             {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
 
             <form onSubmit={submit}>
-                
+
                 <div>
                     <InputLabel htmlFor="email" value="Email" />
 
