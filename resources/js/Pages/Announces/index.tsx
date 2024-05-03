7013824218422fb5cf2aa26da473ad85a3635dcb
Diandreas <davidnjandjeu@gmail.com>
2024-05-03 06:04:37 +0100

    autres pages

---
+++ resources/js/Pages/Announces/index.tsx
@@ -0,0 +1,28 @@
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+
+
+import { Head } from '@inertiajs/react';
+import { PageProps } from '@/types';
+import { Button } from "@/Components/ui/button"
+
+
+
+
+export default function index({ auth }: PageProps) {
+    return (
+        <AuthenticatedLayout
+            user={auth.user}
+            // header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+
+        >
+            <Head title="Dashboard" />
+
+
+
+
+
+
+
+        </AuthenticatedLayout>
+    );
+}
