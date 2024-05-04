3cc5a3a45d1d1bce0271b7a6bb56a5f7b9e71419
Paguem Tagang Michele Sephora <152933754+Mpag10@users.noreply.github.com>
2024-05-03 20:28:16 -0400

    Update Welcome.tsx
    ajout dans la section a propos de nous

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -29,19 +29,42 @@ export default function Welcome({ auth, laravelVersion, phpVersion }: PageProps<
 
 
                 <Head title="Welcome" />
+
                 <Accordion type="single" collapsible className="w-1/4">
         <AccordionItem value="item-1" >
-            <AccordionTrigger>Qui sommes-nous?</AccordionTrigger>
+            <AccordionTrigger>A propos de nous</AccordionTrigger>
             <AccordionContent>
-            Yes. It adheres to the WAI-ARIA design pattern.
+            Nous sommes une équipe passionnée par l'art de présenter les individus sous leur meilleur jour professionnel. 
+            Chez Job CV, nous croyons fermement que chaque personne mérite d'avoir un CV qui reflète fidèlement son parcours,
+             ses compétences et ses aspirations.
             </AccordionContent>
         </AccordionItem>
         </Accordion>
 
 
+        <Accordion type="single" collapsible className="w-1/4">
+        <AccordionItem value="item-1" >
+            <AccordionTrigger>Notre mission </AccordionTrigger>
+            <AccordionContent>
+            Notre mission est de simplifier le processus de création de CV en fournissant des outils intuitifs et des modèles professionnels qui permettent à nos utilisateurs de se démarquer dans leur recherche d'emploi. 
+            Que vous soyez un professionnel chevronné cherchant à évoluer dans votre carrière ou un jeune diplômé débutant dans le monde du travail, 
+            notre plateforme est conçue pour répondre à vos besoins spécifiques.
+            </AccordionContent>
+        </AccordionItem>
+        </Accordion>
 
 
+        <Accordion type="single" collapsible className="w-1/4">
+        <AccordionItem value="item-1" >
+            <AccordionTrigger>Nos valeurs </AccordionTrigger>
+            <AccordionContent>
+            Ce qui nous distingue, c'est notre engagement envers l'excellence et l'innovation.
+         Nous restons constamment à l'écoute des retours de nos utilisateurs afin d'améliorer continuellement notre service et de nous assurer qu'il réponde aux exigences du marché du travail en constante évolution.
+            </AccordionContent>
+        </AccordionItem>
 
+
+        </Accordion>
 
         </AuthenticatedLayout>
         </>
