c1b28e3442bfe0ed710bb5f8e37f6511a673ec78
Diandreas <127276266+Diandreas@users.noreply.github.com>
2024-08-04 00:53:17 +0100

    Update Show.tsx

--- resources/js/Pages/CvInfos/Show.tsx
+++ resources/js/Pages/CvInfos/Show.tsx
@@ -42,7 +42,7 @@ const exportToPdf = () => {
         filename: 'mon_cv_professionnel.pdf',
         image: { type: 'jpeg', quality: 0.98 },
         html2canvas: { scale: 2 },
-        jsPDF: { unit: 'mm', format: 'a4', orientation: 'landscape' }
+        jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
     };
     html2pdf().from(element).set(opt).save();
 };
